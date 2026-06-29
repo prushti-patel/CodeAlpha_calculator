17180119d63a6da6944a3871bc53ef54f8b12470
prushti-patel <prushti0703@gmail.com>
2026-06-29 12:34:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const display = document.getElementById('display');
+
+function appendToDisplay(input) { display.value += input; }
+function clearDisplay() { display.value = ""; }
+function deleteLast() { display.value = display.value.slice(0, -1); }
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = "Error";
+    }
+}
